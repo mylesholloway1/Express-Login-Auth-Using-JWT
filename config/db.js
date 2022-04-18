ba5b3b220491af03757094017bba0e508f340653
mylesholloway1 <mylesholloway1@gmail.com>
2022-04-17 20:39:00 -0400

    connected to mongodb

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongodb');
+
+const ConnectDB = async () => {
+    try {
+        await mongoose.connect(db);
+        console.log('Connected to MongoDB...');
+    } catch (err) {
+        console.log(err.message);
+        //exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = ConnectDB;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const ConnectDB = require('./config/db');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//connect to mongodb
+ConnectDB();
 
 app.get('/', (req,res)=>res.send('hello world, the api is working'));
 
